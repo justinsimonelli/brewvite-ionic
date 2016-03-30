7959162445200063c7410907b6584f080f88c09f
Justin Simonelli <justin.simonelli@gmail.com>
2016-03-30 11:32:16 -0700

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+     
+.controller('whereCtrl', function($scope) {
+
+})
+   
+.controller('whoCtrl', function($scope) {
+
+})
+   
+.controller('whenCtrl', function($scope) {
+
+})
+    
