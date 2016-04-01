058061e5eee0c78ce3f590860f8c636f29b79fc6
Justin Simonelli <justin.simonelli@gmail.com>
2016-04-01 07:59:33 -0700

    Hopefully this is the right way to initialize stuff...

--- www/js/factories/dataFactory.js
+++ www/js/factories/dataFactory.js
@@ -1,50 +1,35 @@
-( function() {
-  'use strict';
+(function() {
+    'use strict';
+    var db = null;
 
-  var DataFactory = function( $log, $http, $cordovaSQLite, $q ) {
-    var db = null,
-    	_sql = null,
-    	tablesInitialized = false;
+    document.addEventListener('deviceready', onDeviceReady, false);
 
-    if( window.cordova ) {
-      db = $cordovaSQLite.openDB( {
-        name: "brewvite.db",
-        location: 1
-      } );
-    } else {
-      db = window.openDatabase( "brewvite.db", "1.0", "brewvite.db", 1024 * 1024 * 100 );
+    function onDeviceReady() {
+        if (window.cordova) {
+            db = window.sqlitePlugin.openDatabase({name: "brewvite.db",iosDatabaseLocation: "default"});
+            db.executeSql("CREATE TABLE IF NOT EXISTS auth_tokens(id integer, provider text primary key, token text, endpoint text, sort integer DEFAULT 0)", [], function (res) {
+              console.log('successfully created table')
+            }, function(error) {
+              console.log('error creating table, error=' + error.message);
+            });
+        } else {
+            db = window.openDatabase("brewvite.db", "1.0", "brewvite.db", 1024 * 1024 * 100);
+        }
     }
 
-    //private
-    var initTables = function(){
-    	$log.debug("initTables running...");
-    	executeSql("CREATE TABLE IF NOT EXISTS auth_tokens(id integer, provider text primary key, token text, endpoint text, sort integer DEFAULT 0)");
-    }
+    var DataFactory = function($log, $http, $cordovaSQLite, $q) {
 
-    var executeSql = function(sql){
-    	$cordovaSQLite.execute( db, sql ).then( function( result ) {
-    		$log.debug(sql);
-    		$log.debug(result)
-    		return result;
-	    }, function( error ) {
-	      $log.error(error);
-	      return error;
-	    } );
-    }
+        // Public
+        var getDB = function() {
+            return db;
+        };
 
-    initTables();
-
-    // Public
-    var getDB = function() {
-      return db;
+        return {
+            getDB: getDB
+        };
     };
 
-    return {
-      getDB: getDB
-    };
-  };
-
-  angular
-    .module( "app" )
-    .factory( "DataFactory", DataFactory );
-}() );
+    angular
+        .module("brewvite")
+        .factory("DataFactory", DataFactory);
+}());
